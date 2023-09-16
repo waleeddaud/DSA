5002e85aba6de33c9a96bb669727313b622378b7
Waleed Daud <waleeddaud3@gmail.com>
2023-09-16 06:42:46 +0500

    Comments added

--- sumArray.cpp
+++ sumArray.cpp
@@ -10,7 +10,7 @@ int main(){
     for(int i=0 ; i<size;i++){
         cin>>arr[i];
     }
-
+    /*This portion calculates the sum of array*/
     int sum=0;
     for(int i=0 ; i<size; i++){
         sum = sum + arr[i];
