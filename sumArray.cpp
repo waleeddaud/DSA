e4402343af484a32cb591b65c1a42c27c3d0e1cd
Waleed Daud <waleeddaud3@gmail.com>
2023-09-16 06:40:45 +0500

    Calculate sum Of an integer array

---
+++ sumArray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cout<<"Enter the size of the array: \n";
+    cin>>size;
+
+    int arr[100];
+    cout<<"Enter "<<size<<" values in array :\n";
+    for(int i=0 ; i<size;i++){
+        cin>>arr[i];
+    }
+
+    int sum=0;
+    for(int i=0 ; i<size; i++){
+        sum = sum + arr[i];
+    }
+    cout<<"The sum of all values of array are: "<<sum;
+
+    return 0;
+}
