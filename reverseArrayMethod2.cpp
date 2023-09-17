b84fd51c01cfe863b91079c0537d661b586df87d
Waleed Daud <waleeddaud3@gmail.com>
2023-09-17 06:57:42 +0500

    added const to size declarator variable

--- reverseArrayMethod2.cpp
+++ reverseArrayMethod2.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int size = 5;
-    int arr[size] = { 4,12,3,88,-3};
+    const int size = 7;
+    int arr[size] = { 4,12,3,88,-3,65,2};
     int brr[size];
     /*It puts the reverse of array in a new empty array of the same size*/
     /*j is new array's index and the value of reversed array is obtained by 
