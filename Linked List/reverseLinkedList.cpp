89662821fc76cb4c22af9c1c9f1a8a18ffc1784f
Waleed Daud <waleeddaud3@gmail.com>
2024-06-20 02:53:24 +0500

    Reverse the Linked List

--- Linked List/reverseLinkedList.cpp
+++ Linked List/reverseLinkedList.cpp
@@ -1,21 +1,21 @@
 //This is the leetcode problem for reversing the linked list
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+  //Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev= NULL;
+        ListNode* prev= nullptr;
         ListNode* curr= head;
-        ListNode* forward= NULL;
-        while(curr!=NULL){
+        ListNode* forward= nullptr;
+        while(curr!=nullptr){
             forward = curr->next;
             curr->next=prev;
             prev = curr;
