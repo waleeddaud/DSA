0bd7ea389669924264bb5c3ba58f9dd1a84039a9
Waleed Daud <waleeddaud3@gmail.com>
2024-06-30 21:29:25 +0500

    Tree: "Depth" , "reverseLevelOrder"

---
+++ Trees/Depth.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+int maxDepth(TreeNode* root) {
+    if(root == nullptr)
+        return 0;
+    int left = maxDepth( root->left );
+    int right = maxDepth( root->right );
+
+    return (max(left,right) + 1);
+
+}
+int main()
+{
+    TreeNode* root = new TreeNode(3);
+    root ->left = new TreeNode(9);
+    root -> right = new TreeNode (20);
+    root ->left->left = new TreeNode (20);
+    root ->left->right = new TreeNode (25);
+    root ->right->left = new TreeNode (15);
+    root ->right->right = new TreeNode (7);
+    cout << maxDepth(root) ;
+    return 0;
+}
