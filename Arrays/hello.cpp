7461b378fa2643e09f7b65a946e62cb4abdad6de
Waleed Daud <waleeddaud3@gmail.com>
2024-09-04 15:55:17 +0500

    hello

---
+++ Arrays/hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<iomanip>
+#include<limits.h>
+#include<vector>
+#include<list>
+#include<queue>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<set>
+using namespace std;
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
